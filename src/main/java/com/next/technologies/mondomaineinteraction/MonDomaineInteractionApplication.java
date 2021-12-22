d08f8f6ab40058eb5297ed4439a539908a162a0d
Namory Traore <namorytraore1984@yahoo.fr>
2021-12-22 17:19:05 -0500

    Premier commit

---
+++ src/main/java/com/next/technologies/mondomaineinteraction/MonDomaineInteractionApplication.java
@@ -0,0 +1,13 @@
+package com.next.technologies.mondomaineinteraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonDomaineInteractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonDomaineInteractionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/next/technologies/mondomaineinteraction/MonDomaineInteractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.technologies.mondomaineinteraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonDomaineInteractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
