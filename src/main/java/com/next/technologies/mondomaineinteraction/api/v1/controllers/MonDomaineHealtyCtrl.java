3cc95cd732850e2ee11bcf1e950b411d9f6d48d1
Namory Traore <namorytraore1984@yahoo.fr>
2021-12-28 13:19:48 -0500

    MAJ

--- src/main/java/com/next/technologies/mondomaineinteraction/api/v1/controllers/MonDomaineHealtyCtrl.java
+++ src/main/java/com/next/technologies/mondomaineinteraction/api/v1/controllers/MonDomaineHealtyCtrl.java
@@ -2,13 +2,14 @@ package com.next.technologies.mondomaineinteraction.api.v1.controllers;
 
 import com.next.technologies.mondomaineinteraction.dtos.HealtyObject;
 import com.next.technologies.mondomaineinteraction.servives.MonDomaineHealtySrv;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = {"service/interaction/mon-domaine"}, produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
@@ -21,18 +22,4 @@ public class MonDomaineHealtyCtrl {
         return healtySrv.getAllServicesHealtyMsg();
     }
 
-    @GetMapping("/sante1")
-    public HealtyObject sanityCheck1() {
-        return healtySrv.getAllServicesHealtyMsg1();
-    }
-
-    @GetMapping("/sante2")
-    public HealtyObject sanityCheck2() {
-        return HealtyObject.builder().serviceInteraction("OK").build();
-    }
-
-    @GetMapping("/sante3")
-    public HealtyObject sanityCheck3() {
-        return healtySrv.getAllServicesHealtyMsg3();
-    }
 }
--- src/main/java/com/next/technologies/mondomaineinteraction/servives/MonDomaineHealtySrv.java
+++ src/main/java/com/next/technologies/mondomaineinteraction/servives/MonDomaineHealtySrv.java
@@ -1,56 +1,27 @@
 package com.next.technologies.mondomaineinteraction.servives;
 
-import static com.next.technologies.mondomaineinteraction.constantes.MonDomaineConst.HEALTHY_MSG;
-
 import com.next.technologies.mondomaineinteraction.dtos.HealtyObject;
-import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
+import static com.next.technologies.mondomaineinteraction.constantes.MonDomaineConst.HEALTHY_MSG;
+
 @Service
 public class MonDomaineHealtySrv {
+
+    @Value("${app.clients.service-metier.url}")
+    private String url;
 
     public HealtyObject getAllServicesHealtyMsg() {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        HealtyObject srvMetierResp =  restTemplate.getForObject("http://localhost:9091/service/metier/mon-domaine/sante", HealtyObject.class);
+        HealtyObject srvMetierResp = restTemplate.getForObject(url, HealtyObject.class);
 
-        HealtyObject.HealtyObjectBuilder builder =  HealtyObject.builder().serviceInteraction(HEALTHY_MSG);
-
-        Optional.ofNullable(srvMetierResp).ifPresent(resp -> {
-            builder.serviceMetier(resp.getServiceMetier());
-        });
-
-        return builder.build();
-    }
-
-    public HealtyObject getAllServicesHealtyMsg1() {
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        HealtyObject srvMetierResp =  restTemplate.getForObject("http://localhost:8081/service/metier/mon-domaine/sante", HealtyObject.class);
-
-        HealtyObject.HealtyObjectBuilder builder =  HealtyObject.builder().serviceInteraction(HEALTHY_MSG);
-
-        Optional.ofNullable(srvMetierResp).ifPresent(resp -> {
-            builder.serviceMetier(resp.getServiceMetier());
-        });
-
-        return builder.build();
-    }
-
-    public HealtyObject getAllServicesHealtyMsg3() {
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        HealtyObject srvMetierResp =  restTemplate.getForObject("http://producer:8081/service/metier/mon-domaine/sante", HealtyObject.class);
-
-        HealtyObject.HealtyObjectBuilder builder =  HealtyObject.builder().serviceInteraction(HEALTHY_MSG);
+        HealtyObject.HealtyObjectBuilder builder = HealtyObject.builder().serviceInteraction(HEALTHY_MSG);
 
         Optional.ofNullable(srvMetierResp).ifPresent(resp -> {
             builder.serviceMetier(resp.getServiceMetier());
