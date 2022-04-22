43807a5f83b61764036ca6dff26ebd7fdbd9ef71
Namory Traore <namorytraore1984@yahoo.fr>
2022-04-22 11:07:17 -0400

    Ajout de comment

--- src/main/java/com/next/technologies/mondomaineinteraction/api/v1/controllers/MonDomaineHealtyCtrl.java
+++ src/main/java/com/next/technologies/mondomaineinteraction/api/v1/controllers/MonDomaineHealtyCtrl.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MonDomaineHealtyCtrl {
 
+    // Injection de service
     private final MonDomaineHealtySrv healtySrv;
 
     @GetMapping("/sante")
